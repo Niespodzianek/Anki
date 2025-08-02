c7f452d53acfc8bd86791230d6d5cfa1eb4ba8aa
Niespodzianek <64963974+Niespodzianek@users.noreply.github.com>
2025-08-02 17:04:36 +0000

    Gotowy skrypt przekształcający słownik do pliku csv

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 slownik = {"house" : "dom", "car" : "samochód", "cat" : "kot", "dog" : "pies"}
-slownik_pd = pd.DataFrame(slownik, index=[0])
-slownik_pd.to_csv("slownik.csv", index=False)
+dane_do_csv = []
+for key, value in slownik.items():
+    dane_do_csv.append({"angielski": key, "polski": value})
+
+print(dane_do_csv)
+
+slowka = pd.DataFrame(dane_do_csv)
+slowka.to_csv("slowka.csv", index=False, encoding="utf-8", header=False, sep="\t")
