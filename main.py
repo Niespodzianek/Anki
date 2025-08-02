0f2050ebb85c2cd4dc6d73c4f0291f5df94617fe
Niespodzianek <64963974+Niespodzianek@users.noreply.github.com>
2025-08-02 16:38:41 +0000

    Pierwsze koty za płoty

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Program pracuje")
+import pandas as pd
+
+slownik = {"house" : "dom", "car" : "samochód", "cat" : "kot", "dog" : "pies"}
+slownik_pd = pd.DataFrame(slownik, index=[0])
+slownik_pd.to_csv("slownik.csv", index=False)
