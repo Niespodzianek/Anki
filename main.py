05cc8e74b010387dcaa9f8cb26119753c9ad32d6
Niespodzianek <michal_grzyb@yahoo.pl>
2025-08-04 16:53:30 +0200

    Dużo zmian
    Wprowadziłem: obsługę flag --help, --versoin, --quiet, --debug, --output i obsługę błędów w przypadku nieprawidłowej nazwy pliku.

--- main.py
+++ main.py
@@ -1,33 +1,91 @@
+import sys, os
+
 import pandas as pd
 
-slownik = {
+APP_NAME: str = "Pomocnik Anki"
+APP_VERSION: str = "0.0.0"
+DEBUG:bool = "--debug" in sys.argv
+QUIET:bool = "--quiet" in sys.argv
+nazwa_pliku_tsv: str = "domyslna_nazwa_pliku_tsv_dla_programu_Anki"
+
+# --------------------------------------------------------------------
+# ----------  PONIŻEJ UMIESZCZAMY, CHWILOWO, KOD SŁOWNIKA  -----------
+# --------------------------------------------------------------------
+# -----------------------------  KOD  --------------------------------
+# --------------------------------------------------------------------
+# ------------------------  KONIEC SŁOWNIKA  -------------------------
+# --------------------------------------------------------------------
+
+slownik: dict[str, str] = {
     "aberration": "odchylenie od normy",
-    "disposition": "dyspozycja / usposobienie",
-    "geotropism": "geotropizm",
-    "inadmissible": "niedopuszczalny",
-    "inevitability": "nieuchronność",
-    "irreversible": "nieodwracalny",
-    "liquidate": "zlikwidować",
-    "non-survival": "brak zdolności przetrwania",
-    "oblong": "podłużny / wydłużony",
-    "penetrated": "przeniknięty",
-    "phylogenic": "filogenetyczny",
-    "postponement": "odroczenie",
-    "reassignment": "przypisanie na nowo",
-    "recalibration": "ponowna kalibracja",
-    "repudiation": "odrzucenie / wyparcie się",
-    "segregated": "odseparowany",
-    "specimen": "egzemplarz / okaz",
-    "stability": "równowaga / trwałość",
-    "submit": "poddawać się / przedstawiać",
-    "televocal": "zdalnie sterowany głosowo"
-}
+    "disposition": "dyspozycja / usposobienie"}
 
-dane_do_csv = []
-for key, value in slownik.items():
-    dane_do_csv.append({"angielski": key, "polski": value})
+def debug_print(tekst: str) -> None:
+    if DEBUG:
+        input(f"DEBUG: {tekst} Naciśnij ENTER aby kontynuować !!!")
+    return None
 
-print(dane_do_csv)
+def info_print(tekst: str):
+    if QUIET:
+        print(f"INFO: {tekst}")
+    return None
 
-slowka = pd.DataFrame(dane_do_csv)
-slowka.to_csv("hello_tomorrow_C2.csv", index=False, encoding="utf-8", header=False, sep="\t")
+def program() -> None:
+    dicts: list[tuple[str, str]] = []
+    lista_do_csv: pd.DataFrame
+    debug_print(f"Oto pythonowy słownik przekształcany do pliku tsv.\n{slownik}")
+    debug_print("Przekształcam słownik do listy.")
+    dicts = list(slownik.items())
+    debug_print(f"Oto lista powstała ze słownika, która teraz zostanie zapisana do pliku tsv.\n{dicts}")
+    debug_print("Tworzę z listy DataFrame.")
+    lista_do_csv = pd.DataFrame(dicts)
+    debug_print("Zapisuje DataFrame do pliku tsv.")
+    lista_do_csv.to_csv(f"{nazwa_pliku_tsv}.tsv", sep="\t", index=False, header=False, encoding="utf-8")
+    debug_print("Plik zapisany, ale jeszcze nie wiem czy poprawnie.")
+    return None
+
+if __name__ == "__main__":
+    if "--help" in sys.argv:
+        os.system("cls" if os.name == "nt" else "clear")
+        print(f"""
+        Pomoc do programu {APP_NAME}:
+
+        Funkcja programu:
+            Program ułatwiający współpracę z programem Anki.
+            Aktualnie, program przekształca słowniki ze słówkami angielskimi i polskimi do plików tsv,
+            celem ich późniejszego dodania do programu Anki.
+            
+        Użycie:
+            --version - wersja programu,
+            --output plik - zapisuje do pliku tsv o nazwie plik.tsv (należy wpisać nazwę pliku bez rozszerzenia tsv),
+            --quiet - uruchomienie programu w trybie z komentarzami,
+            --debug - uruchamianie programu w trybie debug,
+            --help - pomoc.
+        """)
+        sys.exit()
+    if "--version" in sys.argv:
+        os.system("cls" if os.name == "nt" else "clear")
+        print(f"""
+        Program {APP_NAME} - Wersja {APP_VERSION}
+
+        0.0.0
+        Program przekształca słowniki ze słówkami angielskimi i polskimi do plików tsv, celem ich późniejszego
+        dodania do programu Anki.
+        """)
+        sys.exit()
+    if "--quiet" in sys.argv:
+        DEBUG = False
+    if "--output" in sys.argv:
+        index = sys.argv.index("--output")
+        if index + 1 < len(sys.argv):
+            nazwa_pliku_tsv = sys.argv[index + 1]
+            if nazwa_pliku_tsv.endswith(".tsv"):
+                nazwa_pliku_tsv = nazwa_pliku_tsv[:-4]
+        else:
+            debug_print("Błąd, nie podano nazwy pliku !!!")
+            sys.exit(1)
+    os.system("cls" if os.name == "nt" else "clear")
+    info_print("Program pracuje !!!")
+    program()
+    info_print("\nDo zobaczenia !!!")
+    sys.exit()
