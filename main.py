434ee1eb9d040de1f45e436ecfff9452454e8713
Niespodzianek <michal_grzyb@yahoo.pl>
2025-08-05 04:24:17 +0200

    Wersja 1.0.1.
    Poprawka zapobiegająca błędowi związanemu z wpisaniem bezpośrednio po fladze --output innej flagi, z pominięciem nazwy pliku tsv i błędnemu zapisaniu pliku tsv pod nazwą flagi wpisanej po fladze --output.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys, os
 import pandas as pd
 
 APP_NAME: str = "Pomocnik Anki"
-APP_VERSION: str = "1.0.0"
+APP_VERSION: str = "1.0.1"
 DEBUG:bool = "--debug" in sys.argv
 QUIET:bool = "--quiet" in sys.argv
 nazwa_pliku_tsv: str = "domyslna_nazwa_pliku_tsv_dla_programu_Anki"
@@ -47,42 +47,57 @@ def program() -> None:
 if __name__ == "__main__":
     if "--help" in sys.argv:
         os.system("cls" if os.name == "nt" else "clear")
-        print(f"""
-        Pomoc do programu {APP_NAME}:
+        print(f"""Pomoc do programu {APP_NAME}:
 
         Funkcja programu:
             Program ułatwiający współpracę z programem Anki.
             Aktualnie, program przekształca słowniki ze słówkami angielskimi i polskimi do plików tsv,
             celem ich późniejszego dodania do programu Anki.
             
-        Użycie:
+        Flagi:
+            --help - pomoc,
             --version - wersja programu,
+            --history - historia wersji programu,
             --output plik - zapisuje do pliku tsv o nazwie plik.tsv (należy wpisać nazwę pliku bez rozszerzenia tsv),
             --quiet - uruchomienie programu w trybie z komentarzami,
-            --debug - uruchamianie programu w trybie debug,
-            --help - pomoc.
+            --debug - uruchamianie programu w trybie debug.
         """)
         sys.exit()
     if "--version" in sys.argv:
         os.system("cls" if os.name == "nt" else "clear")
-        print(f"""
-        Program {APP_NAME} - Wersja {APP_VERSION}
-
+        print(f"Program {APP_NAME} - Wersja {APP_VERSION}")
+        sys.exit()
+    if "--history" in sys.argv:
+        os.system("cls" if os.name == "nt" else "clear")
+        print(f"""Historia wersji programu {APP_NAME}. Aktualna wersja: {APP_VERSION}.
+        
+        1.0.1
+        Poprawka zapobiegająca błędowi związanemu z wpisaniem bezpośrednio po fladze --output innej flagi, z pominięciem
+        nazwy pliku tsv i błędnemu zapisaniu pliku tsv pod nazwą flagi wpisanej po fladze --output.
+        
         1.0.0
         Program przekształca słownik ze słówkami angielskimi i polskimi do plików tsv, celem ich późniejszego
-        dodania do programu Anki.
+        dodania do programu Anki. Słownik jest w kodzie pliku main.py.
         """)
         sys.exit()
     if "--quiet" in sys.argv:
         DEBUG = False
     if "--output" in sys.argv:
         index = sys.argv.index("--output")
-        if index + 1 < len(sys.argv):
-            nazwa_pliku_tsv = sys.argv[index + 1]
-            if nazwa_pliku_tsv.endswith(".tsv"):
-                nazwa_pliku_tsv = nazwa_pliku_tsv[:-4]
-        else:
-            debug_print("Błąd, nie podano nazwy pliku !!!")
+        try:
+            if sys.argv[index + 1].startswith("--"):
+                print("Uwaga błąd !!! W miejscu symbolu spółki wpisano flagę.")
+                sys.exit(1)
+            if index + 1 < len(sys.argv):
+                nazwa_pliku_tsv = sys.argv[index + 1]
+                if nazwa_pliku_tsv.endswith(".tsv"):
+                    nazwa_pliku_tsv = nazwa_pliku_tsv[:-4]
+            else:
+                debug_print("Błąd, nie podano nazwy pliku !!!")
+                sys.exit(1)
+        except IndexError as error:
+            debug_print(f"Wystąpił błąd związany z wystąpieniem wyjątku: {error}, ale to nie ma znaczenia.")
+            print("Błąd !!! Nie podano nazwy pliku !!!")
             sys.exit(1)
     os.system("cls" if os.name == "nt" else "clear")
     info_print("Program pracuje !!!")
